331d5df01dcdb8b876e313b4dd2a1f02e748284d
Halouiti <1597154@uab.cat>
2026-06-04 21:24:32 +0200

    Added all archives from old repo

--- api/functions/get-user.ts
+++ api/functions/get-user.ts
@@ -6,7 +6,8 @@ import jwt from 'jsonwebtoken';
 export default function handler(req: VercelRequest, res: VercelResponse) {
 
     //1 - Allow CORS
-    const origin = "https://erilllab.github.io" //change in dev
+
+    const origin = "https://collectf.org" //change in dev
 
     res.setHeader("Access-Control-Allow-Origin", origin); // to be changed in prod
     res.setHeader("Access-Control-Allow-Credentials", "true");
